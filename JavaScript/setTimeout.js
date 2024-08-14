1295c413f6aec5f321a0eff2f43e340b8744bcdd
RITIK SINGH <rickysn10x@gmail.com>
2024-08-14 21:35:33 +0530

    new update

--- JavaScript/setTimeout.js
+++ JavaScript/setTimeout.js
@@ -3,5 +3,8 @@ function x()
     var i = 1;
     setTimeout(function () { console.log(i); },5000);// here we set ou time
     // of 5 sec so the output will be printed after 5sec
+    console.log("hello world");// you might think that it will pritn i 
+    // after that it will print hello world but not it will print
+    // hello world first
 }
-x();
+x()
