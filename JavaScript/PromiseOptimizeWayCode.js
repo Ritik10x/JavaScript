5c71e1ad33614ef51dca2a17ce538ed684876dd8
RITIK SINGH <rickysn10x@gmail.com>
2024-08-29 12:48:42 +0530

    Promise Chaining

--- JavaScript/PromiseOptimizeWayCode.js
+++ JavaScript/PromiseOptimizeWayCode.js
@@ -1,18 +1,18 @@
 const cart =["shoes","pants","Kurtas"];
 
-/*
+
 
 createOrder(cart,function(orderId)
  {
     proceedToPayment(OrderID,function(paymentInfp)
 {
-    showOrderSummary(PaymentInp,function(){
+    showOrderSummary(paymentInfo,function(){
         updateWalletBalance();
     });
 });
  });
 
- */
+ 
  
  // this is call BackHell
 
@@ -37,3 +37,13 @@ createOrder(cart).then(Function(OrderId){// this is a optimize way
     proceedToPayment(OrderID);
 
 });
+.then(function(paymentInfo)// we can attach next promise like that without varriable
+{//                      this is called Promise Chaining
+    return showOrdersummary(paymentInfo);
+})//if we have to update balnce also  we can also one more call aback promise
+.then(function(paymentInfo)
+{
+    return updateWalletBalance(PaymentInfo);
+});// people always forget one thing that is return promise 
+// som make sure dont forget to return thats how data keep flowing
+//because function will retun thats how function work
