e34b005c6017a34f9cf82649a8ca093288fa0b05
RITIK SINGH <rickysn10x@gmail.com>
2024-09-07 12:59:32 +0530

    complex problem in promise chain

---
+++ JavaScript/PromiseChainThatReplacesCallBackHell.js
@@ -0,0 +1,68 @@
+const cart = ["shoes","pants","kurtas"];
+ createOrder(cart)// once we get the orderId from this function createOrder 
+ .then(function(orderId)// then we can pass it to the next function To proceedToPayment
+{                               
+    console.log(orderId);
+    return orderId;// return to down to the function
+})
+
+
+.then(function(orderId){///      this is called PromiseCHaining
+   return  proceedToPayment(orderId);// the main thing we always forget is to return
+})
+// this function will go down in the paymentinfp
+
+.then(function(paymentinfo)
+{
+    console.log(paymentinfo);
+})
+
+
+// if  there is any Error the catch will handle any error in the chain
+
+.catch(function(err)
+{
+    console.log(err.message);
+})// we can use catch for hadnle just the first function error to if occurs firstly 
+// just put the catch there
+.then(function(orderId)// this function will be called coz i attached
+{
+    console.log("no matter what happen it will be called");
+})
+
+
+
+
+function createOrder(orderId)
+{
+        const pr = new Promise(function(resolve,reject)
+    {
+            if(!validatecart(cart))
+        {
+                const err = new Error("cart is not valid");
+                reject(err);
+        }
+             orderId = "12345";
+        if(orderId)
+        {
+            setTimeout(function()
+            {
+                 resolve(orderId)
+            },5000);
+        }
+    }) ;                  return pr;
+}
+
+function proceedToPayment(orderId)// instead of what we up there created promises we can aslo 
+                        // create promise like that
+{
+    return new Promise(function(resolve,reject)
+{
+    resolve("payment successful");
+})
+}
+
+function validatecart()
+{
+    return true;
+}
