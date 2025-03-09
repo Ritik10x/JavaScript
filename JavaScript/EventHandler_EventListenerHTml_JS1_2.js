0082815c2217f9ea6cc911cb081c99cb426297f4
RITIK SINGH <rickysn10x@gmail.com>
2025-03-09 18:54:07 +0530

    2nd file for event Handle and Listener

---
+++ JavaScript/EventHandler_EventListenerHTml_JS1_2.js
@@ -0,0 +1,4 @@
+document.getElementById("clickMe").//this will get the element with the id of clickMe 
+addEventListener("click", function() {})//inside the "()" there is a callback function
+//this function will be called when the element with the id of clickMe is clicked
+// callback function make surenthe it will defenate run after it.
