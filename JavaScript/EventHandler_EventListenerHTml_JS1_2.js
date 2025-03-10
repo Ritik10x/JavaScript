624a1257f369eb5827e1f8fc9164386048e701b4
RITIK SINGH <rickysn10x@gmail.com>
2025-03-10 11:48:21 +0530

    new change

--- JavaScript/EventHandler_EventListenerHTml_JS1_2.js
+++ JavaScript/EventHandler_EventListenerHTml_JS1_2.js
@@ -1,4 +1,4 @@
 document.getElementById("clickMe").//this will get the element with the id of clickMe 
 addEventListener("click", function() {})//inside the "()" there is a callback function
 //this function will be called when the element with the id of clickMe is clicked
-// callback function make surenthe it will defenate run after it.
+// callback function make sure it will defenate run after it.
