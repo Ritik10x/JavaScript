0ab46e729c6b710120508aaa500709169c64dbfb
RITIK SINGH <rickysn10x@gmail.com>
2025-03-17 11:04:40 +0530

    Arrow Function method

---
+++ JavaScript/HOF_prob_solv_map_using_Arrow_fun.js
@@ -0,0 +1,6 @@
+const radius = [1, 2, 3, 4];
+
+console.log(radius.map( y =>2 * Math.PI* y));
+console.log(radius.map(x =>
+   Math.PI * x * x));
+ // the same code here i passed the whole  function isnside the the map
