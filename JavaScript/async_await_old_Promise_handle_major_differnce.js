f53135a97e63be1e0b1b9bf89afecb08113510b7
RITIK SINGH <rickysn10x@gmail.com>
2025-03-27 23:36:15 +0530

    Differnce b/w Async await and old promise handle

---
+++ JavaScript/async_await_old_Promise_handle_major_differnce.js
@@ -0,0 +1,40 @@
+ // differnce b/w async await and old promise handler
+const p = new Promise(function (resolve,reject){ 
+    setTimeout(function(){
+        return resolve ("Promsie resolved value");
+    },10000)
+    
+})
+
+// new await async method
+
+async function handlePromise() {    
+    const val = await p;// await will wait here to promise to resolve after that it will
+    console.log("NamsteJavaScript");// got to next line, in old method it didnt work like that 
+    console.log(val);// javascript is waiting  in const val = await p; here 
+}
+
+handlePromise();
+ 
+ /* old tradational method
+
+ javascript will directly go to next Line but in async await it will wait till promise and bypass the Javascipt engine rule
+ that javascript wait for none
+ function getData()
+{
+
+p.then(function(result){
+console.log(result);
+
+ })                                 
+ console.log("Namaste JavaScript")
+}
+getData();
+
+
+                                        
+*/ /** output will be 
+Namste javascript
+ Promsie resolved value  // after 10 sec 
+
+ */
