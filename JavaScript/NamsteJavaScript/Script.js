1cc77056bfb778a2ca600aacbb283be6496257ee
RITIK SINGH <rickysn10x@gmail.com>
2025-03-31 12:19:59 +0530

    Html and script page

---
+++ JavaScript/NamsteJavaScript/Script.js
@@ -0,0 +1,20 @@
+/*
+Topic learn before starting react 
+
+Tenarrry operator
+
+Arrays and Objects
+. Array Destructuring
+● Object Destructuring
+⚫ rest operator
+spread operator
+
+Event Listeners
+⚫ onClick
+⚫ onBlur
+⚫ onChange
+⚫ onFocus
+setTimeout() & setInterval()
+
+event bubling and captureing in javaScript
+*/
